b0510378c74349027751a30e6f827cc27c1f81a4
Madhav Kruthiventy <kruthiventym@gmail.com>
2020-08-15 11:47:50 +1200

    store votes in list

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+db = []
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/vote', function(req, res, next) {
-  console.log(req.body.vote)
+  vote = req.body.vote
+  console.log(vote)
+  db.push(vote)
+  console.log(db)
 });
 
 module.exports = router;
